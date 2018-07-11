skiselkov/OpenWXR

commit b5283529816e47a34716c4de9940fc1292c04719

"smooth" is a reserved word in later GLSL version and AMD drivers are being dumb.

src/atmo_xp11.c
@@ -553,7 +553,7 @@ transfer_new_efis_frame(void)
 	glUniform1i(glGetUniformLocation(xp11_atmo.smooth_prog, "tex"), 0);
 	glUniform2f(glGetUniformLocation(xp11_atmo.smooth_prog, "tex_sz"),
 	    EFIS_WIDTH, EFIS_HEIGHT);
-	glUniform1f(glGetUniformLocation(xp11_atmo.smooth_prog, "smooth"),
+	glUniform1f(glGetUniformLocation(xp11_atmo.smooth_prog, "smooth_val"),
 	    WX_SMOOTH_RNG / range);
 	glutils_draw_quads(&xp11_atmo.efis_quads, xp11_atmo.smooth_prog);
 
@@ -706,6 +706,8 @@ atmo_xp11_init(const char *xpdir, const char *plugindir)
 	    DEFAULT_VTX_ATTRIB_BINDINGS, NULL);
 	lacf_free(path);
 	lacf_free(path2);
+	if (xp11_atmo.cleanup_prog)
+		goto errout;
 
 	path = mkpathname(xpdir, plugindir, "data", "generic.vert", NULL);
 	path2 = mkpathname(xpdir, plugindir, "data", "smooth.frag", NULL);
@@ -713,12 +715,17 @@ atmo_xp11_init(const char *xpdir, const char *plugindir)
 	    DEFAULT_VTX_ATTRIB_BINDINGS, NULL);
 	lacf_free(path);
 	lacf_free(path2);
+	if (xp11_atmo.smooth_prog == 0)
+		goto errout;
 
 	mutex_init(&xp11_atmo.lock);
 
 	XPLMRegisterDrawCallback(update_cb, xplm_Phase_Gauges, 0, NULL);
 
 	return (&atmo);
+errout:
+	atmo_xp11_fini();
+	return (NULL);
 }
 
 void

data/smooth.frag
@@ -20,7 +20,7 @@
 
 uniform sampler2D	tex;
 uniform vec2		tex_sz;
-uniform float		smooth;
+uniform float		smooth_val;
 
 void
 main(void)
@@ -30,8 +30,8 @@ main(void)
 
 	for (int i = -2; i <= 2; i += 1) {
 		for (int j = -2; j <= 2; j += 1) {
-			intens += texture2D(tex, vec2(pos.x + i * smooth,
-			    pos.y + j * smooth));
+			intens += texture2D(tex, vec2(pos.x + i * smooth_val,
+			    pos.y + j * smooth_val));
 		}
 	}
 

commit 7453cefba270986ef02f643f496b80f4633a4ac7

Commit b528352 introduced a regression.

src/atmo_xp11.c
@@ -643,11 +643,13 @@ atmo_xp11_init(const char *xpdir, const char *plugindir)
 	inited = B_TRUE;
 
 	memset(&xp11_atmo, 0, sizeof (xp11_atmo));
+	mutex_init(&xp11_atmo.lock);
 
 	debug_cmd = XPLMCreateCommand("openwxr/debug_atmo_xp11",
 	    "Dump XP11 screenshot into X-Plane folder");
 	ASSERT(debug_cmd != NULL);
 	XPLMRegisterCommandHandler(debug_cmd, debug_cmd_handler, 0, NULL);
+	XPLMRegisterDrawCallback(update_cb, xplm_Phase_Gauges, 0, NULL);
 
 	for (int i = 0; i < 3; i++) {
 		fdr_find(&drs.cloud_type[i],
@@ -706,7 +708,7 @@ atmo_xp11_init(const char *xpdir, const char *plugindir)
 	    DEFAULT_VTX_ATTRIB_BINDINGS, NULL);
 	lacf_free(path);
 	lacf_free(path2);
-	if (xp11_atmo.cleanup_prog)
+	if (xp11_atmo.cleanup_prog == 0)
 		goto errout;
 
 	path = mkpathname(xpdir, plugindir, "data", "generic.vert", NULL);
@@ -717,10 +719,6 @@ atmo_xp11_init(const char *xpdir, const char *plugindir)
 	lacf_free(path2);
 	if (xp11_atmo.smooth_prog == 0)
 		goto errout;
-
-	mutex_init(&xp11_atmo.lock);
-
-	XPLMRegisterDrawCallback(update_cb, xplm_Phase_Gauges, 0, NULL);
 
 	return (&atmo);
 errout:
@@ -736,6 +734,7 @@ atmo_xp11_fini(void)
 	inited = B_FALSE;
 
 	XPLMUnregisterCommandHandler(debug_cmd, debug_cmd_handler, 0, NULL);
+	XPLMUnregisterDrawCallback(update_cb, xplm_Phase_Gauges, 0, NULL);
 
 	if (xp11_atmo.pbo != 0)
 		glDeleteBuffers(1, &xp11_atmo.pbo);
@@ -750,7 +749,6 @@ atmo_xp11_fini(void)
 	glutils_destroy_quads(&xp11_atmo.efis_quads);
 
 	mutex_destroy(&xp11_atmo.lock);
-	XPLMUnregisterDrawCallback(update_cb, xplm_Phase_Gauges, 0, NULL);
 }
 
 void